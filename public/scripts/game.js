42426d3ff48210c722e314b6f8a1001a0456d7ef
Lorne Davies <thisislorne@gmail.com>
2016-07-30 15:43:11 +1200

    writing happiness variable on screen

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -3,19 +3,31 @@ var game = new Phaser.Game(window.innerWidth, window.innerHeight,
                  preload: preload, create: create, update: update
              });
 
+// variables yo
+var happinessText; //Phaser.text object that displays the text
+var happiness = 50; //variable for bird happiness
+// variables yo
+
 function preload(){
 	// load all sprites and assets here
-      // game.load.image('logo', '/images/logo.png');  
+    // game.load.image('logo', '/images/logo.png');  
 }
 
 function create(){
-  	// add them to the page
-      // game.add.sprite(10, 100, 'logo');
-      // game.stage.backgroundColor = '#e7e7e7';
+  // add them to the page
+    // game.add.sprite(10, 100, 'logo');
+
+    game.stage.backgroundColor = '#000'; //set background colour
+
+  happinessText = game.add.text(16, 16, 'happiness: ' + happiness, { fontSize: '32px', fill: '#e7e7e7' }); //create text object, and place on stage
 }
 
 function update(){
-    // add them to the page
-      // game.add.sprite(10, 100, 'logo');
-      // game.stage.backgroundColor = '#e7e7e7';
+  // add them to the page
+    // game.add.sprite(10, 100, 'logo');
+    // game.stage.backgroundColor = '#e7e7e7';
+
+  // each tick update happines by 10 and write to text on screen
+  happiness += 10;
+  happinessText.text = 'happiness: ' + happiness;
 }
