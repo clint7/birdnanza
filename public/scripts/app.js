2435064fa113b7de5ab5d2de9d4251fc958d46bb
Bjarne Frederiksen <frederiksen.bjarne@gmail.com>
2016-07-31 16:04:44 +1200

    video lightbox

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,10 @@
 // this is for all non game logic
 jQuery(document).ready(function($) {
+
+	// scrolling nav
 	$("a[rel='scroll']").mPageScroll2id();
+
+	// video lightbox
+	$('.swipebox-video').swipebox();
 });
 
